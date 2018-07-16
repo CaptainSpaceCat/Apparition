7f33802523ad12c3b65882bdf37f8b77ebe9d3cd
Daniel <danieldo.do4@gmail.com>
2018-07-15 18:47:20 -0700

    added buttons

---
+++ Apparition/Assets/Scripts/button.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour {
+
+	public bool isPressed = false; //public for testing purposes
+	public float regHeight = .15f;
+	public float pressedHeight = .05f;
+
+	void Update () {
+		Vector3 scale = gameObject.transform.localScale;
+		if (isPressed) {
+			scale.y = pressedHeight;
+			gameObject.transform.localScale = scale;
+		} else {
+			scale.y = regHeight;
+			gameObject.transform.localScale = scale;
+		}
+	}
+
+	public void changeState(bool pressed) {
+		isPressed = pressed;
+	}
+}
