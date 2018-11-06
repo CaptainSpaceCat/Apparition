c7c6eff1c859f3dffd5221e14fb66ddab99b28a5
Connor Settle <csettle@stanford.edu>
2018-11-05 22:01:57 -0800

    Update and rename PlayerController.cs to PController.cs

--- New/PController.cs
+++ New/PController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PController : MonoBehaviour {
 
 	public Vector3 teleportDestination;
 
